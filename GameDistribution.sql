ee56dbc26cdd89773f21497d8050c7b2eadaf230
GITJIWOO <kjw011231@gmail.com>
2021-09-14 18:17:57 +0900

    create sql query

---
+++ GameDistribution.sql
@@ -0,0 +1,43 @@
+CREATE TABLE game (
+    
+);
+
+CREATE TABLE user (
+
+);
+
+CREATE TABLE userpicture (
+
+);
+
+CREATE TABLE gamereview (
+
+);
+
+CREATE TABLE gamepicture (
+
+);
+
+CREATE TABLE friends (
+
+);
+
+CREATE TABLE qna (
+
+);
+
+CREATE TABLE userpayment (
+
+);
+
+CREATE TABLE userlibrary (
+
+);
+
+CREATE TABLE gamewishlist (
+
+);
+
+CREATE TABLE shoppingbasket (
+
+);
