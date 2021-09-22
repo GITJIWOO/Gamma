4b59758b2393015889e42bb52068f143af6f1617
PYOJITAE <kjw011231@gmail.com>
2021-09-22 18:19:36 +0900

    create review mapper

--- GameDistribution/src/main/java/org/game/review/mapper/ReviewMapper.java
+++ GameDistribution/src/main/java/org/game/review/mapper/ReviewMapper.java
@@ -1,5 +1,24 @@
 package org.game.review.mapper;
 
-public interface ReviewMapper {
+import java.util.List;
 
+import org.game.review.domain.ReviewVO;
+
+public interface ReviewMapper {
+	
+	// 평가 조회
+	public List<ReviewVO> ReviewList();
+	
+	// 평가 상세 조회
+	public ReviewVO reviewSelect();
+	
+	// 평가 작성
+	public void reviewInsert(ReviewVO vo); 
+	
+	// 평가 수정
+	public void reviewUpdate(ReviewVO vo);
+	
+	// 평가 삭제
+	public void reviewDelete(long gnum);
+	
 }
