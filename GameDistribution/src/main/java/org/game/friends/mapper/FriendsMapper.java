5429dc1097993a73bfeb2a3b75bc48f8b9959fb0
jane <choiwodls@naver.com>
2021-09-22 10:31:33 +0900

    modify

--- GameDistribution/src/main/java/org/game/friends/mapper/FriendsMapper.java
+++ GameDistribution/src/main/java/org/game/friends/mapper/FriendsMapper.java
@@ -1,5 +1,19 @@
 package org.game.friends.mapper;
 
-public interface FriendsMapper {
+import java.util.List;
 
+import org.game.friends.domain.FriendsVO;
+
+public interface FriendsMapper {
+	// 친구 추가(insert-selectKey)
+	public void insertFriend(FriendsVO vo);
+	
+	// 친구 목록 조회(select)
+	public List<FriendsVO> getFList(String keyword);
+	
+	// 친구 여부(select)
+	public FriendsVO getFriend(String following);
+	
+	// 친구 삭제(delete)
+	public void deleteFriend(String follower);
 }
