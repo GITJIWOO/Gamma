08a83f04cf405fd4cb77fdf9a5fad2c14cfd9b6b
DESKTOP-088GRHQ\sos10 <sos104v@naver.com>
2021-09-24 16:56:09 +0900

    0924커밋

--- GameDistribution/src/main/java/org/game/user/controller/UserController.java
+++ GameDistribution/src/main/java/org/game/user/controller/UserController.java
@@ -1,5 +1,21 @@
 package org.game.user.controller;
 
+import org.game.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/user/*")
+@AllArgsConstructor
 public class UserController {
+	@Autowired
+	private UserService service;
+	
+	
 
 }
