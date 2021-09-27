70abe3cac37c16ed68bd9cec4b716e9874052420
DESKTOP-088GRHQ\sos10 <sos104v@naver.com>
2021-09-27 17:29:13 +0900

    0927커밋

--- GameDistribution/src/main/java/org/game/user/controller/UserController.java
+++ GameDistribution/src/main/java/org/game/user/controller/UserController.java
@@ -1,8 +1,14 @@
 package org.game.user.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.game.user.domain.ConsumerVO;
 import org.game.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +22,25 @@ public class UserController {
 	@Autowired
 	private UserService service;
 	
+	@PostMapping("/userLogin")
+public String login(ConsumerVO userVO, HttpServletRequest request) {
+        
+        ConsumerVO userVO = service.userSearch(userVO);
+        HttpSession session = request.getSession();
+        session.setAttribute("userVO", userVO);
+        
+        return "userLogin";
+    }
+	
+	  @PostMapping("/userLogout")
+	    public String userLogout(@ModelAttribute("userVO") ConsumerVO userVO, HttpServletRequest request) {
+	        
+	        service.userLogout(userVO);
+	        HttpSession session = request.getSession();
+	        session.invalidate();
+	        
+	        return "redirect:/user/userLogin";
+	    }
 	
 
 }
