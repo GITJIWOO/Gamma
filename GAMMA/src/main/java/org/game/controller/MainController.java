cb0cbebb444a6da9fac35e17ed85b9971d24b7b0
PYOJITAE <kjw011231@gmail.com>
2021-10-16 02:28:31 +0900

    modify

--- GAMMA/src/main/java/org/game/controller/MainController.java
+++ GAMMA/src/main/java/org/game/controller/MainController.java
@@ -1,6 +1,12 @@
 package org.game.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.game.user.domain.ConsumerVO;
+import org.game.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,8 +19,21 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class MainController {
 	
+	@Autowired
+	private UserMapper userMapper;
+	
 	@GetMapping("/main")
-	public String gammaMain() {
+	public String gammaMain(HttpSession session, Model model) {
+		
+		String cid = (String)session.getAttribute("session_cid");
+		
+		if(cid != null) {
+			ConsumerVO consumer = userMapper.userGet(cid);
+			model.addAttribute("consumer", consumer);
+		}
+		
+		model.addAttribute("cid", cid);
+		
 		return "/main/main";
 	}
 }
