e7f9a25735df35d9fe72ecb2e66586aa10c74104
PYOJITAE <kjw011231@gmail.com>
2021-10-18 01:50:23 +0900

    modify

--- GAMMA/src/main/java/org/game/user/controller/ConsumerBasketController.java
+++ GAMMA/src/main/java/org/game/user/controller/ConsumerBasketController.java
@@ -28,9 +28,9 @@ public class ConsumerBasketController {
 	private ConsumerBasketService service;
 	
 	@PostMapping("/basketList")
-	public String getBasketList(@RequestParam("cid") String cid, HttpSession session, Model model) {
+	public String getBasketList(HttpSession session, Model model) {
 		
-		cid = (String)session.getAttribute("session_cid");
+		String cid = (String)session.getAttribute("session_cid");
 		
 		if(cid == null) {
 			return "redirect:/user/userLogin";
@@ -68,7 +68,7 @@ public class ConsumerBasketController {
 		
 		service.removeConsumerBasket(cid, gnum);
 		
-		return "redirect:/gamepayment/basketList";
+		return "redirect:/gamepayment/basketList?cid=" + cid;
 	}
 	
 }
