1d6d2f4590d7f2a8a9f8238f2252fbfc65e9d7d3
PYOJITAE <kjw011231@gmail.com>
2021-10-17 20:39:53 +0900

    modify

--- GAMMA/src/main/java/org/game/gamelibrary/service/GameLibraryService.java
+++ GAMMA/src/main/java/org/game/gamelibrary/service/GameLibraryService.java
@@ -2,12 +2,16 @@ package org.game.gamelibrary.service;
 
 import java.util.List;
 
+import org.game.gamelibrary.domain.GameLibraryVO;
 import org.game.gamelibrary.domain.ResultLibraryVO;
 
 public interface GameLibraryService {
 	
 	// 라이브러리 조회
 	public List<ResultLibraryVO> getAllConsumerLibrary(String cid);
+	
+	// 라이브러리 게임 하나 조회
+	public GameLibraryVO getOneConsumerLibrary(String cid, long gnum);
 	
 	// 라이브러리 게임 추가
 	public void additionalLibrary(String cid, long gnum);
