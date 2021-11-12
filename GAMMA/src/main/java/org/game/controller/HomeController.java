8e8f25d1fcacba1a761d2a42388b657cc5e461b2
git-jane-hub <choiwodls@naver.com>
2021-11-12 14:33:56 +0900

    add websocket2

--- GAMMA/src/main/java/org/game/controller/HomeController.java
+++ GAMMA/src/main/java/org/game/controller/HomeController.java
@@ -4,10 +4,14 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
+@RequestMapping("/friends/*")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -34,6 +39,13 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@GetMapping("/chatting")
+	public String chat(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception{
+		
+		
+		return "/friends/chat";
 	}
 	
 }
